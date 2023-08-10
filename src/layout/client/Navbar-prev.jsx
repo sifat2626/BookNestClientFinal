2acb06c3a2587d75d6cff833217fd8fb7c58565c
sifat2626 <ashrafulsifat26@gmail.com>
2023-08-10 21:06:17 +0600

    Update Navbar-prev.jsx

--- src/layout/client/Navbar-prev.jsx
+++ src/layout/client/Navbar-prev.jsx
@@ -36,7 +36,7 @@ const NavbarPrev = () => {
   }
   const fetchAuthors = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/v1/writers");
+      const response = await axios.get("https://book-nest-backend.onrender.com/api/v1/writers");
       setAuthors(response.data);
     } catch (error) {
       console.error("Error fetching authors:", error.message);
@@ -45,7 +45,7 @@ const NavbarPrev = () => {
   const fetchPublication = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:5000/api/v1/publications"
+        "https://book-nest-backend.onrender.com/api/v1/publications"
       );
       setPublications(response.data);
     } catch (error) {
