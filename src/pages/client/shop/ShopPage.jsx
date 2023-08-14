d72663921628c4a6e0c331f8390900de70eaad88
sifat2626 <ashrafulsifat26@gmail.com>
2023-08-14 21:26:30 +0600

    Update ShopPage.jsx

--- src/pages/client/shop/ShopPage.jsx
+++ src/pages/client/shop/ShopPage.jsx
@@ -1,46 +1,46 @@
-import React, { Fragment, useState } from "react";
-import FilterPanel from "./FilterOption.jsx";
-
-import Books from "../Books.jsx";
-
-
-const ShopPage = () => {
-  // const [selectedCategory, setSelectedCategory] = useState(null);
-  // const [selectedPublication, setSelectedPublication] = useState(null);
-  // const [selectedAuthor, setSelectedAuthor] = useState(null);
-  // const [minPrice, setMinPrice] = useState("");
-  // const [maxPrice, setMaxPrice] = useState("");
-  // const [sort, setSort] = useState("");
-
-  // console.log(selectedCategory,selectedPublication,selectedAuthor,minPrice,maxPrice);
-
-
- 
-  return (
-    <Fragment>
-      <div className="page-content mt-4">
-        <div className="container">
-          <div className="row">
-            <aside className="col-lg-3 col-4 order-lg-first">
-              <FilterPanel/>
-            </aside>
-            <div className="col-lg-9 col-8">
-              <Books
-                // selectedCategory={selectedCategory}
-                // selectedPublication={selectedPublication}
-                // selectedAuthor={selectedAuthor}
-                // minPrice={minPrice}
-                // maxPrice={maxPrice}
-                // sort={sort}
-                // setSort={setSort}
-              />
-              
-            </div>
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
-};
-
-export default ShopPage;
+// import React, { Fragment, useState } from "react";
+// import FilterPanel from "./FilterOption.jsx";
+//
+// import Books from "../Books.jsx";
+//
+//
+// const ShopPage = () => {
+//   // const [selectedCategory, setSelectedCategory] = useState(null);
+//   // const [selectedPublication, setSelectedPublication] = useState(null);
+//   // const [selectedAuthor, setSelectedAuthor] = useState(null);
+//   // const [minPrice, setMinPrice] = useState("");
+//   // const [maxPrice, setMaxPrice] = useState("");
+//   // const [sort, setSort] = useState("");
+//
+//   // console.log(selectedCategory,selectedPublication,selectedAuthor,minPrice,maxPrice);
+//
+//
+//
+//   return (
+//     <Fragment>
+//       <div className="page-content mt-4">
+//         <div className="container">
+//           <div className="row">
+//             <aside className="col-lg-3 col-4 order-lg-first">
+//               <FilterPanel/>
+//             </aside>
+//             <div className="col-lg-9 col-8">
+//               <Books
+//                 // selectedCategory={selectedCategory}
+//                 // selectedPublication={selectedPublication}
+//                 // selectedAuthor={selectedAuthor}
+//                 // minPrice={minPrice}
+//                 // maxPrice={maxPrice}
+//                 // sort={sort}
+//                 // setSort={setSort}
+//               />
+//
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </Fragment>
+//   );
+// };
+//
+// export default ShopPage;
